433de06d1a3404c27fbb26ff7a7b480e05f9c58b
Subin Park <98893360+psbin1217@users.noreply.github.com>
2024-01-02 01:20:22 +0900

    [Bronze V] Title: 꼬마 정민, Time: 128 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
@@ -0,0 +1,5 @@
+var input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+var c = parseInt(input[2]);
+console.log(a + b + c);
